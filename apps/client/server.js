9c7022318ed16585ed327ffc7159a932ce8c608d
Nguyễn Ngô Vũ Nhân <vunhan2492001@gmail.com>
2026-02-26 11:00:00 +0700

    update audit log

--- apps/client/server.js
+++ apps/client/server.js
@@ -20,7 +20,7 @@ app.use('/api', createProxyMiddleware({
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // SPA fallback - serve index.html for all other routes
-app.get('*', (req, res) => {
+app.get(/^(.*)$/, (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
