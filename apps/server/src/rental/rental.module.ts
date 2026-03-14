1b0afb4b9efa9792046f3ca2dfd4ca7247b153ef
Nguyễn Ngô Vũ Nhân <vunhan2492001@gmail.com>
2026-03-14 17:55:17 +0700

    fix Dockerfile

--- apps/server/src/rental/rental.module.ts
+++ apps/server/src/rental/rental.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RentalService } from './rental.service';
 import { RentalController } from './rental.controller';
+import { Vehicle } from './entities/vehicle.entity';
+import { VehicleRental } from './entities/vehicle-rental.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Vehicle, VehicleRental])],
   providers: [RentalService],
-  controllers: [RentalController]
+  controllers: [RentalController],
 })
 export class RentalModule {}
