995b123c0952e7002b83cd8b59aa65f8689d4002
Nguyễn Ngô Vũ Nhân <vunhan2492001@gmail.com>
2026-07-11 14:16:03 +0700

    update

--- apps/server/src/user/enum/role.ts
+++ apps/server/src/user/enum/role.ts
@@ -2,4 +2,11 @@ export enum ROLE {
   ADMIN = 'admin',
   INTERNAL_USER = 'internal_user',
   CUSTOMER = 'customer',
+  HOTEL_OWNER = 'hotel_owner',
+  HOTEL_MANAGER = 'hotel_manager',
+  FRONT_DESK = 'front_desk',
+  HOUSEKEEPING = 'housekeeping',
+  MAINTENANCE = 'maintenance',
+  LAUNDRY = 'laundry',
+  WAREHOUSE = 'warehouse',
 }
